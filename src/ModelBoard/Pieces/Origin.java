6b54d8d2a436d69e1f09c3ba1d26592e289b8d89
Mathieu Regnard <mathieu.regnard2@gmail.com>
2017-03-06 19:04:14 +0100

    Refactored Origin to match blocks refactoring

--- src/ModelBoard/Pieces/Origin.java
+++ src/ModelBoard/Pieces/Origin.java
@@ -5,30 +5,23 @@ import ModelBoard.Position.Position;
 /**
  * Created by Irindul on 15/02/2017.
  */
-public class Origin {
+public class Origin { //TODO Refactor with only an attribute position in block
 
-    private Block block;
+
     private Position position;
 
 
-    public Origin(Block block, Position position) {
-        this.block = block;
+    public Origin(Position position) {
         this.position = position;
     }
 
     public Origin(Origin o){
-        this(new Block(o.block), new Position(o.position));
+        this(new Position(o.position));
     }
 
-    public Block getBlock() {
-        return block;
-    }
 
     public Position getPosition() {
         return position;
     }
 
-    public Position getPositionOfBlock(){
-        return block.getPosition(position.getX(), position.getY());
-    }
 }
