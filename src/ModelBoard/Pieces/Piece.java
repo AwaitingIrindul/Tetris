824e3d35af656aee1cbe24fad1ad846919c0db46
Mathieu Regnard <mathieu.regnard2@gmail.com>
2017-03-10 12:19:32 +0100

    Added some methods

--- src/ModelBoard/Pieces/Piece.java
+++ src/ModelBoard/Pieces/Piece.java
@@ -5,27 +5,27 @@ import ModelBoard.Direction;
 import ModelBoard.Position.Position;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
-import java.util.Map;
-import java.util.function.Consumer;
 
 /**
  * Created by Irindul on 09/02/2017.
  */
-public class BlockAggregate {
+public class Piece implements RemovalListener{
 
     private Position position;
     private Origin origin;
     private int height, width;
     private boolean positions[][];
 
+    private ArrayList<RemovalListener> listeners;
 
-    public BlockAggregate(int height, int width) {
+
+    public Piece(int height, int width) {
         position = new Position(0, 0);
         this.height = height;
         this.width = width;
         positions = new boolean[height][width];
+        listeners = new ArrayList<>();
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
                 positions[i][j] = false;
@@ -33,11 +33,7 @@ public class BlockAggregate {
         }
     }
 
-    public void removePosition(Position pos){
-        positions[pos.getX() - position.getX()][pos.getY() - position.getY()] = false;
-    }
-
-    public BlockAggregate(String[][] scheme, Position startingPosition){
+    public Piece(String[][] scheme, Position startingPosition){
         this(scheme.length, scheme[0].length);
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
@@ -52,18 +48,25 @@ public class BlockAggregate {
 
     }
 
-    public BlockAggregate(BlockAggregate b){
+    public Piece(Piece b){
         this.position = b.position;
         height = b.height;
         width = b.width;
         positions = new boolean[width][height];
-
+        listeners = new ArrayList<>();
+        listeners.addAll(b.listeners);
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
                 positions[i][j] = b.positions[i][j];
             }
         }
         this.origin = new Origin(b.origin);
+    }
+
+
+    public void removePosition(Position pos){
+        positions[pos.getX() - position.getX()][pos.getY() - position.getY()] = false;
+        listeners.forEach(RemovalListener::onDown);
     }
 
     public boolean checkRotation(Grid g){
@@ -74,6 +77,25 @@ public class BlockAggregate {
         }*/
 
         return true;
+    }
+
+    public List<Position> getLowers(){
+        List<Position> positions = new ArrayList<>();
+        int maxHeight = 0;
+        boolean oneLineNotEmpty = false;
+        for (int i = height-1; i >= 0; i--) {
+            for (int j = 0; j < width; j++) {
+                if(this.positions[i][j]){
+                    oneLineNotEmpty = true;
+                    positions.add(new Position(position.getX() + i, position.getY() + j));
+                }
+            }
+            if(oneLineNotEmpty)
+                break;
+        }
+
+        return positions;
+
     }
 
     public void rotateClockWise(){
@@ -112,12 +134,23 @@ public class BlockAggregate {
     public int getMinimumY(){
 
         List<Position> positions = getPositions();
-        return positions.stream().max((o1, o2) -> Integer.compare(o1.getY(), o2.getY())).get().getY();
+        return positions.stream().min((o1, o2) -> Integer.compare(o1.getY(), o2.getY())).get().getY();
 
     }
 
 
     public Position getPosition(){
         return position;
+    }
+
+    public void addListener(RemovalListener l){
+        listeners.add(l);
+    }
+
+    @Override
+    public void onDown() {
+        this.move(Direction.DOWN);
+        listeners.stream().filter(listener -> listener != this)
+                  .forEach(RemovalListener::onDown);
     }
 }
