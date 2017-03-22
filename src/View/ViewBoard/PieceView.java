01a0b46096dc2e0bf1f2e9e5034b689fb7f6497a
Irindul <mathieu.regnard2@gmail.com>
2017-03-22 13:45:42 +0100

    Cleaned up

--- src/View/ViewBoard/PieceView.java
+++ src/View/ViewBoard/PieceView.java
@@ -29,8 +29,8 @@ public class PieceView {
 
         piece.getPositions().stream()
                 .map(position -> {
-                    Rectangle rect = new Rectangle(tilesize, tilesize, color);
-                    rect.relocate(position.getY()*tilesize, position.getX()*tilesize);
+                    Rectangle rect = new Rectangle(this.tilesize, this.tilesize, this.color);
+                    rect.relocate(position.getY()*this.tilesize, position.getX()*this.tilesize);
                     return rect;
                 })
                 .forEach(rectangle -> square.add(rectangle));
@@ -41,7 +41,6 @@ public class PieceView {
 
     public synchronized void update(){
 
-        //square.forEach(rectangle -> square.remove(rectangle));
         square.clear();
         square = new ArrayList<>();
 
@@ -55,58 +54,16 @@ public class PieceView {
                 .forEach(rectangle -> square.add(rectangle));
     }
 
-
-    public void drawNext(GraphicsContext g){
-        g.setFill(color);
-
-        int minY = piece.getMinimumY();
-
-        piece.getPositions().forEach(pos -> {
-            int x, y;
-            x = pos.getY();
-            y = pos.getX();
-
-            x-=minY;
-            y+=1;
-            g.fillRect(x*tilesize, y*tilesize, tilesize, tilesize);
-        });
-
-    }
-
     public List<Rectangle> getSquare() {
         return square;
-    }
-
-    // TODO: 15/03/2017 add offset in parameter (-2 for tetris, 0 for blocks ect..)
-    public void draw(Color c, Color s, GraphicsContext g){
-        g.setFill(c);
-        g.setStroke(s);
-
-        piece.getPositions().forEach(pos -> {
-            int x, y;
-            x = pos.getY();
-            y = pos.getX(); //TODO Refactor with normal sense;
-            y-=offset;
-            g.strokeRect(x*tilesize, y*tilesize, tilesize, tilesize);
-            g.fillRect(x*tilesize, y*tilesize, tilesize, tilesize);
-        });
-
     }
 
     public Piece getPiece() {
         return piece;
     }
 
-    public void undraw(GraphicsContext g) {
-
-       piece.getPositions().forEach(pos -> {
-           int x, y;
-           x = pos.getY();
-           y = pos.getX();
-
-           y-=offset;
-
-           g.clearRect(x*tilesize, y*tilesize, tilesize+0.5, tilesize+0.5);
-       });
+    public Color getColor(){
+        return color;
     }
+
 }
