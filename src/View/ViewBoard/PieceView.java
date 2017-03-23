2a66d6b02f50d746d8387e646afb1107c6266413
Irindul <mathieu.regnard2@gmail.com>
2017-03-23 10:21:52 +0100

    Added coloration

--- src/View/ViewBoard/PieceView.java
+++ src/View/ViewBoard/PieceView.java
@@ -14,15 +14,21 @@ import java.util.List;
 public class PieceView {
 
     private Color color;
+    private Color stroke;
     private Piece piece;
     private double tilesize;
     private int offset;
 
     private List<Rectangle> square;
 
-    public PieceView(Color color, Piece block, double tilesize, int offset) {
+    public PieceView(Color color, Piece piece, double tilesize, int offset) {
+        this(color, piece, tilesize, offset, Color.BLACK);
+
+    }
+
+    public PieceView(Color color, Piece piece, double tilesize, int offset, Color stroke){
         this.color = color;
-        this.piece = block;
+        this.piece = piece;
         this.tilesize = tilesize;
         this.offset = offset;
         square = new ArrayList<>();
@@ -34,7 +40,7 @@ public class PieceView {
                     return rect;
                 })
                 .forEach(rectangle -> square.add(rectangle));
-
+        this.stroke = stroke;
     }
 
 
@@ -48,7 +54,7 @@ public class PieceView {
                 .map(position -> {
                     Rectangle rect = new Rectangle(tilesize, tilesize, color);
                     rect.relocate(position.getY()*tilesize, (position.getX()-offset)*tilesize);
-                    rect.setStroke(Color.BLACK);
+                    rect.setStroke(stroke);
                     return rect;
                 })
                 .forEach(rectangle -> square.add(rectangle));
